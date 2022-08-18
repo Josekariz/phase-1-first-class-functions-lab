12da53b28e4a54b90338b5769d8e5142d4b90123
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-18 22:14:04 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function (drivers) {
+  const firstTwoArray = [...drivers];
+  return [firstTwoArray[0], firstTwoArray[1]];
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  const lastTwoArray = [...drivers];
+  return [lastTwoArray[2], lastTwoArray[3]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+  return function (fares) {
+    return fares ** 2;
+  };
+}
+
+const fareDoubler = (fares) => {
+  return fares * 2;
+};
+
+const fareTripler = (fares) => {
+  return fares * 3;
+};
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  if (selectingDrivers === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+  } else if ( selectingDrivers === returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers);
+  }
+}
